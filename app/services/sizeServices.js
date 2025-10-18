2f6dc00b80399115ef919accef43be9c46ae6674
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-10-17 21:41:57 -0300

    Controlador del modelo Size/BACK-03

---
+++ app/services/sizeServices.js
@@ -0,0 +1,60 @@
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+export const createSize = async ({
+  size,
+}) => {
+  const Size = await prisma.size.create({
+    data: {
+      size,
+      is_deleted: false,
+    },
+  });
+
+  return Size;
+};
+
+export const listSizes = async () => {
+  return prisma.size.findMany({
+    where: { is_deleted: false }
+  });
+};
+
+export const searchSizeById = async (id) => {
+  const foundSize = await prisma.size.findUnique({ where: { id } });
+  if (!foundSize) {
+    const err = new Error("No existe talle con ese ID");
+    err.status = 404;
+    throw err;
+  }
+  return foundSize;
+};
+
+export const deleteSize = async (id) => {
+  const foundSize = await prisma.size.findUnique({ where: { id } });
+  if (!foundSize) {
+    const err = new Error("No existe talle con ese ID");
+    err.status = 404;
+    throw err;
+  }
+
+  return prisma.size.update({
+    where: { id },
+    data: { is_deleted: true },
+  });
+};
+
+export const updateSize = async ({ id, data }) => {
+  const foundSize = await prisma.size.findUnique({ where: { id } });
+  if (!foundSize) {
+    const err = new Error("No existe talle con ese ID");
+    err.status = 404;
+    throw err;
+  }
+
+  return prisma.size.update({
+    where: { id },
+    data,
+  });
+};
