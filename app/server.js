6f287e0dba8f8daab6826f70c95a18f8cfcdb71a
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-11-01 18:06:54 -0300

    implementacion de cors para el front/BACK-06

--- app/server.js
+++ app/server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors"
 const app = express();
 import { PORT } from "./globals/globalConstans.js";
 import routes_init from "./routes/index.routes.js";
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
 
 app.use("/api", routes_init());
 
